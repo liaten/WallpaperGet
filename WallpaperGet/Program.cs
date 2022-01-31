18adad14c1a9a6df02ec682b72fbe5dd11013a81
liaten <vlad171vlad@gmail.com>
2022-01-31 20:05:51 +0300

    Не определяются параметры рабочего стола

---
+++ WallpaperGet/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Net;
+namespace WallpaperGet
+{
+    public static class Program
+    {
+        public static string path = AppDomain.CurrentDomain.BaseDirectory;
+        public static string GetTimeStamp()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+        }
+        private static string GetURL(short width, short height)
+        {
+            return $"https://source.unsplash.com/random/{width}x{height}";
+        }
+        private static string URL = "";
+        private static string TimeStamp = "";
+        private static string ImageDir = "";
+        private static string ImageFullName = "";
+        public static void Download(WebClient OuterWebClient, short width, short height)
+        {
+            URL = GetURL(width, height);
+            TimeStamp = GetTimeStamp();
+            ImageDir = path + "Image";
+            ImageFullName = $"{ImageDir}/{width}x{height}_{TimeStamp}.jpg";
+            if (Directory.Exists(ImageDir))
+            {
+                //Console.WriteLine($"Directory already exists at directory: {ImageDir}");
+                using WebClient client = OuterWebClient;
+                client.DownloadFile(new Uri(URL), ImageFullName);
+                //Console.WriteLine("The image was saved successfully at {0}.", Directory.GetCreationTime(ImageFullName));
+            }
+            else
+            {
+                Directory.CreateDirectory(ImageDir);
+                //Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(ImageDir));
+                using WebClient client = OuterWebClient;
+                client.DownloadFile(new Uri(URL), ImageFullName);
+                //Console.WriteLine("The image was saved successfully at {0}.", Directory.GetCreationTime(ImageFullName));
+            }
+        }
+        public static void Main()
+        {
+            /*
+             * System.Windows.SystemParameters.PrimaryScreenWidth
+             * System.Windows.SystemParameters.PrimaryScreenHeight
+            */
+            short width = (short)System.Windows.SystemParameters.PrimaryScreenWidth;
+            short height = (short)System.Windows.SystemParameters.PrimaryScreenHeight;
+            try { Download(OuterWebClient: new(), height, width); }
+            catch { }
+            try
+            {
+                WallpaperManager.Set(ImageFullName, WallpaperManager.Style.Tiled);
+                //Console.WriteLine("The wallpaper was set successfully.");
+            }
+            catch { }
+            while (File.Exists(ImageFullName))
+            {
+                try
+                {
+                    File.Delete(ImageFullName);
+                    //Console.WriteLine("The image was deleted successfully.");
+                }
+                catch { }
+            }
+            /*
+            Console.Write("Press the Q Key to escape out of program: ");
+            while (Console.ReadKey(true).Key != ConsoleKey.Q)
+            {
+                continue;
+            }
+            */
+        }
+    }
+}
---
+++ WallpaperGet/WallpaperManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Microsoft.Win32;
+
+namespace WallpaperGet
+{
+    public class WallpaperManager
+    {
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched
+        }
+
+        public static void Set(string imgPath, Style style)
+        {
+            var img = Image.FromFile(Path.GetFullPath(imgPath));
+            string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+            img.Save(tempPath, ImageFormat.Bmp);
+
+            var key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+
+            switch (style)
+            {
+                case Style.Tiled:
+                    key.SetValue(@"WallpaperStyle", 1.ToString());
+                    key.SetValue(@"TileWallpaper", 1.ToString());
+                    break;
+                case Style.Centered:
+                    key.SetValue(@"WallpaperStyle", 1.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                    break;
+                case Style.Stretched:
+                    key.SetValue(@"WallpaperStyle", 2.ToString());
+                    key.SetValue(@"TileWallpaper", 0.ToString());
+                    break;
+                default:
+                    break;
+            }
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, tempPath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+}
